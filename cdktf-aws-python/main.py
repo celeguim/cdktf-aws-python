df5e33318dbf4d60904f49cf02b395af3e68faf0
Luiz H. Celeguim <luiz.celeguim@gmail.com>
2022-01-29 21:25:28 +0100

    python updates

--- cdktf-aws-python/main.py
+++ cdktf-aws-python/main.py
@@ -15,24 +15,22 @@ class MyStack(TerraformStack):
         # AWS Provider on North Virginia (us-east-1)
         AwsProvider(self, "Aws", region="us-east-1")
 
+        # MyVPC on 10.0.0.0/16
         vpc = Vpc(self, "MyVPC", cidr_block="10.0.0.0/16", tags={'Name': 'MyVPC'})
-        TerraformOutput(self, "main_route_table_id", value=vpc.main_route_table_id)
 
-        # subnets
+        # Public Subnet on 10.0.1.0/24
         public_subnet = Subnet(self, 'public_subnet', cidr_block='10.0.1.0/24', vpc_id=vpc.id, tags={'Name': 'public_subnet'})
+
+        # Private Subnet on 10.0.2.0/24
         private_subnet = Subnet(self, 'private_subnet', cidr_block='10.0.2.0/24', vpc_id=vpc.id, tags={'Name': 'private_subnet'})
 
-        # internet gateway
+        # Internet Gateway
         igw = InternetGateway(self, 'igw', vpc_id=vpc.id, tags={'Name': 'igw'})
 
         public_route_table = RouteTable(self, id='public_route_table', vpc_id=vpc.id, tags={"Name": "public_route_table"})
-        # public_rta1 = RouteTableAssociation(self, 'igw_rta', route_table_id=public_route_table.id, gateway_id=igw.id)
         public_rta2 = RouteTableAssociation(self, 'public_subnet_rta', subnet_id=public_subnet.id, route_table_id=public_route_table.id)
-        # public_rtr = RouteTableRoute(cidr_block='10.0.1.0/24', gateway_id=igw.id)
-        # public_route = Route(self, 'public_route', destination_cidr_block='10.0.1.0/24')
         public_route = Route(self, 'public_route', route_table_id=public_route_table.id, gateway_id=igw.id,
                              destination_cidr_block='0.0.0.0/0')
-        # public_route_table.route = [public_route]
 
         # NAT Gateway - needed for private network internet access
         # We also need an Elastic IP
@@ -40,12 +38,11 @@ class MyStack(TerraformStack):
         nat_gateway = NatGateway(self, 'nat_gateway', subnet_id=public_subnet.id, tags={"Name": "nat_gateway"})
         nat_gateway.allocation_id = eip.allocation_id
 
+        # Private Route Table / nat_gateway association
         private_route_table = RouteTable(self, id='private_route_table', vpc_id=vpc.id, tags={"Name": "private_route_table"})
         RouteTableAssociation(self, 'private_subnet_rta', subnet_id=private_subnet.id, route_table_id=private_route_table.id)
         Route(self, 'private_route', route_table_id=private_route_table.id, nat_gateway_id=nat_gateway.id,
               destination_cidr_block='0.0.0.0/0')
-
-        # MainRouteTableAssociation()
 
         # Public Security Group
         pub_sec_gr = SecurityGroup(self, "pub_sec_gr", description="Public Security Group", name="pub_sec_gr", vpc_id=vpc.id,
@@ -72,16 +69,15 @@ class MyStack(TerraformStack):
         priv_sec_gr.ingress = [priv_sec_gr_ing1, priv_sec_gr_ing2, priv_sec_gr_ing3]
         priv_sec_gr.egress = [priv_sec_gr_eg1]
 
-        # Public Instance on Public Subnet - SSH + HTTP
+        # Public Instance on Public Subnet
         # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type - ami-08e4e35cccc6189f4 (64-bit x86)
         pub_instance = Instance(self, "pub_instance", ami="ami-08e4e35cccc6189f4", instance_type="t2.micro", subnet_id=public_subnet.id,
                                 key_name="celeghin.mac13", depends_on=[pub_sec_gr], associate_public_ip_address=True,
                                 tags={"Name": "pub_instance"})
         pub_instance.security_groups = [pub_sec_gr.id]
         TerraformOutput(self, "public_ip", value=pub_instance.public_ip)
-        # TerraformOutput(self, "pub_instance.security_groups", value=pub_instance.security_groups)
 
-        # Private Instance on Private Subnet - NAT Gateway
+        # Private Instance on Private Subnet
         # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type - ami-08e4e35cccc6189f4 (64-bit x86)
         priv_instance = Instance(self, "priv_instance", ami="ami-08e4e35cccc6189f4", instance_type="t2.micro", subnet_id=private_subnet.id,
                                  key_name="celeghin.mac13", associate_public_ip_address=False, tags={"Name": "priv_instance"})
